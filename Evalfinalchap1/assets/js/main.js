8dbba80f1fe79d2fdd02163684994425b65e386d
NiicoTrax <nicolaslegaye@live.fr>
2024-05-06 15:43:41 +0200

    good

--- Evalfinalchap1/assets/js/main.js
+++ Evalfinalchap1/assets/js/main.js
@@ -117,17 +117,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-
-  function aos_init() {
-    AOS.init({
-      duration: 1000,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    });
-  }
-  window.addEventListener('load', () => {
-    aos_init();
-  });
-
 });
