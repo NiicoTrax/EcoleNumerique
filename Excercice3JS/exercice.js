916fec150b0bf6d6a03253246c9ac8cd5d864d13
NiicoTrax <nicolaslegaye@live.fr>
2024-05-06 16:50:27 +0200

    Good

--- Excercice3JS/exercice.js
+++ Excercice3JS/exercice.js
@@ -7,3 +7,5 @@ document.getElementById('myDiv3').innerHTML = "Je suis affiché";
 /* Le code suivant va masquer l'élément myDiv3 */
 
 document.getElementById('myDiv3').style.display='none';
+document.getElementById('myDiv2').style.display='none';
+document.getElementById('myDiv').style.display='none';
