bccbbe7cd4065309342c5ac48be928947628f39c
NiicoTrax <nicolaslegaye@live.fr>
2024-05-06 16:55:15 +0200

    Good

---
+++ Excercice5JS/exercice.js
@@ -0,0 +1,6 @@
+
+var myVar  = 'Bonjour cher inconnu !';
+
+/* Le code suivant va modifier la propriété innerHTML de l'élément myDiv et lui attribuer la valeur de myVar */
+
+document.getElementById('myDiv').innerHTML = myVar;
