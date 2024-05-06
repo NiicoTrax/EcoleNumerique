eac7dbf1ea087cbc6418b361aa279c261cb46cc5
NiicoTrax <nicolaslegaye@live.fr>
2024-05-06 16:53:09 +0200

    Good

---
+++ Excercice4JS/exercice.js
@@ -0,0 +1,8 @@
+
+document.getElementById('myDiv').innerHTML = "Je suis affiché";
+document.getElementById('myDiv2').innerHTML = "Je suis affiché";
+document.getElementById('myDiv3').innerHTML = "Je suis affiché";
+
+/* Le code suivant va afficher l'élément myDiv */
+
+document.getElementById('myDiv').style.display='block';
