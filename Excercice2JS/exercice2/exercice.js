18402f1f3ce9a03a07dd79a0d32d24828bfc10ef
NiicoTrax <nicolaslegaye@live.fr>
2024-05-06 16:48:45 +0200

    Good

---
+++ Excercice2JS/exercice2/exercice.js
@@ -0,0 +1,12 @@
+
+
+/* Modifiez le code suivant pour changer le innerHTML de myDiv2 */
+document.getElementById('myDiv2').innerHTML = "Bonjour je m'appelle Amandine";
+
+document.getElementById('myDiv2').style.color='red';
+/* Le code suivant va modifier la propriété style.color de myDiv3 , le texte qu'il contient sera affiché en rouge
+ */
+
+document.getElementById('myDiv').innerHTML= "Comment ça va ?";
+
+document.getElementById('myDiv').style.color='blue';
---
+++ Excercice3JS/exercice.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById('myDiv').innerHTML = "Je suis affiché";
+document.getElementById('myDiv2').innerHTML = "Je suis affiché";
+document.getElementById('myDiv3').innerHTML = "Je suis affiché";
+
+/* Le code suivant va masquer l'élément myDiv3 */
+
+document.getElementById('myDiv3').style.display='none';
