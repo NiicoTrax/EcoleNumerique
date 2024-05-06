06d4af29f191bf3650dd924d8f7c305b6027e9e7
NiicoTrax <nicolaslegaye@live.fr>
2024-05-06 16:43:24 +0200

    Good

---
+++ Excercice2JS/exercice.js
@@ -0,0 +1,8 @@
+
+/* Modifiez le code suivant pour changer le innerHTML de myDiv2 */
+document.getElementById('myDiv').innerHTML = "Texte affiché dans myDiv";
+
+/* Le code suivant va modifier la propriété style.color de myDiv3 , le texte qu'il contient sera affiché en rouge
+ */
+
+document.getElementById('myDiv3').style.color='red';
