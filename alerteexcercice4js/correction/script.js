0c51e6610a5c3905998643983fc12ea934b225f0
NiicoTrax <nicolaslegaye@live.fr>
2024-05-07 14:09:35 +0200

    ok

---
+++ alerteexcercice4js/correction/script.js
@@ -0,0 +1,6 @@
+
+    var nom = prompt('nom'); 
+    var prenom = prompt('prenom'); 
+    var ville = prompt ('ville');
+
+alert ('Nom : ' + nom + '\nPrenom : ' + prenom + '\nVille : ' + ville + '\n');
