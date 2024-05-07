6bf62d3da15c2d04ea28e865f69c2fe6d372964d
NiicoTrax <nicolaslegaye@live.fr>
2024-05-07 14:08:20 +0200

    ok

--- alerteexcercice4js/script.js
+++ alerteexcercice4js/script.js
@@ -1,1 +1,5 @@
-//votre code ici
+var nom = prompt ("Quel est votre nom ?");
+var prenom = prompt ("Quel est votre prénom ?");
+var ville = prompt ("Quel est votre ville ?");
+
+alert('Nom : ' + nom + '\nPrenom : '+ prenom + '\nVille : ' + ville + '\n')
