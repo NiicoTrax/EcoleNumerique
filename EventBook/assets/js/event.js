93e0e8841fee9daffd20a06343c010f609ecb804
NiicoTrax <nicolaslegaye@live.fr>
2024-05-07 14:32:24 +0200

    ok

--- EventBook/assets/js/event.js
+++ EventBook/assets/js/event.js
@@ -1,1 +1,5 @@
+
 const eventName = "Nom de l'événement stocké dans une variable" ;
+const eventDescription = "Description de l'événement" ;
+const ticketType1 = "Billet classique" ;
+const ticketType2 = "Second Tarif" ;
