3bccb788c558e251e692957b84d461a0237e93c9
NiicoTrax <nicolaslegaye@live.fr>
2024-05-14 10:19:59 +0200

    Good

--- Enonce_DOM_ex1_JS/exercice1.js
+++ Enonce_DOM_ex1_JS/exercice1.js
@@ -1,14 +1,24 @@
+document.body.onload = addElement;
+
+function addElement() {
+  var newDiv = document.createElement("div");
+  
+  var newContent = document.createTextNode("Ce div a été ajouté en utilisant javascript");
+
+  newDiv.appendChild(newContent);
+
+  var currentDiv = document.getElementById("div1");
+  document.body.insertBefore(newDiv, currentDiv);
+}
 
 
-var monDiv = .createElement('div');
 
 
 document.getElementById('viewport').appendChild(monDiv);
 
 
-
-
-
+var divSupprime = document.getElementById ('aSupprimer');
+document.body.removeChild(divSupprime);
 
 
 
