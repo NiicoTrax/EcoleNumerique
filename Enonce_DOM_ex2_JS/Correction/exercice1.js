2d945e3ed1d96f89638cf5f46c86ca7905cc2616
NiicoTrax <nicolaslegaye@live.fr>
2024-05-14 11:14:00 +0200

    Good

---
+++ Enonce_DOM_ex2_JS/Correction/exercice1.js
@@ -0,0 +1,12 @@
+
+var FirtLast = document.getElementById('id-name');
+
+FirtLast.innerHTML = 'Joe Black';
+
+
+
+
+
+
+
+
---
+++ Enonce_DOM_ex2_JS/exercice1.js
@@ -0,0 +1,17 @@
+
+var FirtLast = document.getElementById('id-name');
+
+FirtLast.innerHTML = 'LEGAYE Nicolas';
+
+
+
+
+
+
+
+
+
+
+
+
+
