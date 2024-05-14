06a8bcf40fe6d9916203aaabbc3de6c499625a7d
NiicoTrax <nicolaslegaye@live.fr>
2024-05-14 10:53:29 +0200

    Good

--- Enonce_DOM_ex1_JS/Correction/exercice1.js
+++ Enonce_DOM_ex1_JS/Correction/exercice1.js
@@ -1,21 +1,26 @@
-var monDiv = document.createElement('div1');
+
+
+var monDiv = document.createElement('div');
 
 document.getElementById('viewport').appendChild(monDiv);
 
 monDiv.innerHTML = "Ce div a été ajouté en utilisant du javascript";
 
 
+
 var MonDeuxiemeDiv = document.createElement('div');
 
 document.getElementById('viewport').appendChild(MonDeuxiemeDiv);
 
-MonDeuxiemeDiv.style.backgroundColor = 'blue';
+MonDeuxiemeDiv.style.backgroundColor = 'red';
 
 MonDeuxiemeDiv.style.width = '100px';
 
 MonDeuxiemeDiv.style.height = '100px';
 
 
+
 var Supprime = document.getElementById('aSupprimer');
 
 document.getElementById('viewport').removeChild(Supprime);
+
