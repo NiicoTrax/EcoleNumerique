f204c062a82b78ae7e8a24e55ed74de4e6441a00
NiicoTrax <nicolaslegaye@live.fr>
2024-05-14 09:32:46 +0200

    Good

--- EventBook/assets/js/event.js
+++ EventBook/assets/js/event.js
@@ -1,6 +1,6 @@
 const eventName = "Nom de l'événement stocké dans une variable" ;
 const eventDescription = "Description de l'événement" ;
-const ticketType1 = "Billet classique" ;
+const ticketType1 = "Billet Classique" ;
 const ticketType2 = "Billet VIP" ;
 const ticketType1Price = 35 ;
 const ticketType2Price = 65 ;
