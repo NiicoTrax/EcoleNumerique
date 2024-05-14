5232b021b240a5f7fa25798f25a6f6dabf076f94
NiicoTrax <nicolaslegaye@live.fr>
2024-05-14 12:00:27 +0200

    Good

--- EventBook/assets/js/event.js
+++ EventBook/assets/js/event.js
@@ -1,5 +1,5 @@
 const eventName = "Nom de l'événement stocké dans une variable" ;
-const eventDescription = "Description de l'événement" ;
+const eventDescription = "Description de l'événement stocké dans une variable" ;
 const ticketType1 = "Billet Classique" ;
 const ticketType2 = "Billet VIP" ;
 const ticketType1Price = 35 ;
@@ -9,11 +9,38 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const eventID = urlParams.get('id');
 
-const displayTicket = function(ticketType, ticketPrice)
-{
-    return  `<tr>
-                <td>${ticketType}</td>
-                <td>${ticketPrice}€</td>
-                <td><a href="purchaseTickets.html" class="event-button">Acheter</a></td>
-            </tr>`;
+const domEventTitle = document.querySelector('h2');
+domEventTitle.innerHTML = eventName;
+
+const domEventDescription = document.getElementById('event-description');
+domEventDescription.innerHTML = eventDescription;
+
+const domEventTicketsTable = document.querySelector('table')
+
+const displayTicket = function(ticketType, ticketPrice){
+
+    let tr = document.createElement('tr');
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    let td3 = document.createElement('td');
+
+    td1.innerHTML = ticketType;
+    td2.innerHTML = ticketPrice;
+    td3.innerHTML = '<a href="purchaseTickets.html" class="event-button">Acheter</a>'
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+
+    domEventTicketsTable.appendChild(tr);
+}
+
+
+if(eventID == 1)
+    {
+        displayTicket("Billet Classique", 35);
+        displayTicket("Billet VIP", 65);
+    }
+else {
+        displayTicket("Billet classique", 35);
 }
