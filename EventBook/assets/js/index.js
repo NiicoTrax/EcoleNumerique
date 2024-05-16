2340f9b8cfec08b31f0bbec7ab8a68c28e55defe
NiicoTrax <nicolaslegaye@live.fr>
2024-05-16 11:57:48 +0200

    Good

--- EventBook/assets/js/index.js
+++ EventBook/assets/js/index.js
@@ -3,15 +3,33 @@ const eventCategoryLi= eventCategoryContainer.getElementsByTagName('li');
 
 eventCategoryLi[0].addEventListener('click', function(event) {
     this.classlist.toggle('active');
+    if(tempElement != this )
+
+        {
+            tempElement.classList.toggle('active');
+        }
+
 });
 
 eventCategoryLi[1].addEventListener('click', function(event) {
-    this.classlist.toggle('active');
     let tempElement = eventCategoryContainer.querySelector('.active');
+    if(tempElement)
+        {
+            tempElement.classList.toggle('active');
+        }
+
+
+    this.classlist.toggle('active');
 });
 
 eventCategoryLi[2].addEventListener('click', function(event) {
-    this.classlist.toggle('active');
     let tempElement = eventCategoryContainer.querySelector('.active');
+    if(tempElement)
+        {
+            tempElement.classList.toggle('active');
+        } 
+
+
+    this.classlist.toggle('active');
 });
 
