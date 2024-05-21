adc4130785827771e95902c7d44356e6da26d78a
NiicoTrax <nicolaslegaye@live.fr>
2024-05-21 10:58:51 +0200

    Good

--- Switchexcercice1/Correction/exercice1.js
+++ Switchexcercice1/Correction/exercice1.js
@@ -1,13 +1,17 @@
 var fruits = "Apple";
-
 switch (fruits) {
 
     case "Banana":
+
         alert("Hello");
+
     break;
 
+
     case "Apple":
+
         alert("Welcome");
+
     break;
 
 }
--- Switchexcercice2/Correction/exercice1.js
+++ Switchexcercice2/Correction/exercice1.js
@@ -1,13 +1,23 @@
-var fruits = "Apple";
-
+var fruits = "Pomme";
 switch (fruits) {
-
+    
     case "Banana":
+
         alert("Hello");
+
     break;
+
 
     case "Apple":
+
         alert("Welcome");
+
     break;
 
+
+    default :
+
+        alert("aucun des deux");
+
+    break;
 }
