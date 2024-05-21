c92f2159bcac33482878cc3bd1393e29b92d16f5
NiicoTrax <nicolaslegaye@live.fr>
2024-05-21 10:09:49 +0200

    Good

---
+++ Conditionsexercice3/exercice3.js
@@ -0,0 +1,31 @@
+
+
+/* Modifier le code ci dessous pour réaliser l'exercice */
+
+var x = 5;
+var y =5;
+
+if(x == y)
+{
+    document.getElementById('monDiv').innerHTML = "La variable x est égale à la variable y";
+}
+
+else if (x < y)
+{
+    document.getElementById('monDiv').innerHTML = "La variable x est inférieur  à la variable y";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
