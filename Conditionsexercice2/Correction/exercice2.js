343ed61883fe8bc369a6c688034f67ff02cc703e
NiicoTrax <nicolaslegaye@live.fr>
2024-05-21 10:01:04 +0200

    Good

---
+++ Conditionsexercice2/Correction/exercice2.js
@@ -0,0 +1,12 @@
+
+
+/* Modifier le code ci dessous pour réaliser l'exercice */
+var x = 3;
+var y = 1;
+
+if (x < y) {
+    document.getElementById('monDiv').innerHTML = "La variable x est supérieure à la variable y";
+} else {
+    document.getElementById('monDiv').innerHTML = "La variable x n'est pas supérieure à la variable y";
+}
+
