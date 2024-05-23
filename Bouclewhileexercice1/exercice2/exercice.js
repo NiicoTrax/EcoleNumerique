4ef4a211009eab580a9a193649bd31efb15c470e
NiicoTrax <nicolaslegaye@live.fr>
2024-05-23 10:55:56 +0200

    Good

---
+++ Bouclewhileexercice1/exercice2/exercice.js
@@ -0,0 +1,17 @@
+
+
+var catPic = document.getElementById("cat-pic");
+var onCatClick = function(e) {
+    var stashePic = document.getElementById("mustache-pic");
+    stashePic.style.top = "100px";
+    stashePic.style.left = "100px";
+};
+catPic.addEventListener("click", onCatClick);
+
+
+
+
+
+
+
+
