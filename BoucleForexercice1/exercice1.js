0d803aafa4ebd3829860948151b9501d7bec91dc
NiicoTrax <nicolaslegaye@live.fr>
2024-05-23 09:31:39 +0200

    Good

---
+++ BoucleForexercice1/exercice1.js
@@ -0,0 +1,11 @@
+
+
+
+/* Modifier le code ci dessous pour réaliser l'exercice */
+
+var i;
+( = ;  < ; ) {
+    document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i+"<br><br>";
+    }
+
+
