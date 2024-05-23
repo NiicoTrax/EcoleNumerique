525bd14d51fda018575fb9e44db542a8529bcbb6
NiicoTrax <nicolaslegaye@live.fr>
2024-05-23 11:14:18 +0200

    Good

---
+++ Bouclewhileexercice1/2. While/Exercice 2/Correction/exercice2.js
@@ -0,0 +1,12 @@
+
+
+
+/* Modifier le code ci dessous pour réaliser l'exercice */
+
+var i = 0;
+while (i < 10) {
+    document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i;
+    i = i + 2 ;
+}
+
+
