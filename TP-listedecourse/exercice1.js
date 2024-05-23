7bcabfc954a0617fbe3f14e813897712047294d7
NiicoTrax <nicolaslegaye@live.fr>
2024-05-23 16:48:51 +0200

    Good

---
+++ TP-listedecourse/exercice1.js
@@ -0,0 +1,23 @@
+
+
+// Inutile de modifier le code suivant
+var articles = [
+    "Orangina",
+    "Creme Fraiche",
+    "Tartiflette",
+    "Emmental",
+    "Bananes",
+    "Chips",
+    "Bieres",
+    "Pizza"
+];
+
+// Votre code à partir d'ici :
+
+
+
+
+
+
+
+
