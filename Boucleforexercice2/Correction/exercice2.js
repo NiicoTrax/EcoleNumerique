edda40961604a0d685361d9c520787213de6992a
NiicoTrax <nicolaslegaye@live.fr>
2024-05-23 09:43:30 +0200

    Good

---
+++ Boucleforexercice2/Correction/exercice2.js
@@ -0,0 +1,11 @@
+
+
+/* Modifier le code ci dessous pour réaliser l'exercice */
+
+
+for( var i =56 ; i > 0 ; i = i - 28 )
+{
+    document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i+"<br><br>";
+}
+
+
