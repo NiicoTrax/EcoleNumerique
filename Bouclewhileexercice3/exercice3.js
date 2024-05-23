4a03cc740e5c288aaa3377ed8d8415615bbf007e
NiicoTrax <nicolaslegaye@live.fr>
2024-05-23 11:23:03 +0200

    Good

--- Bouclewhileexercice3/exercice3.js
+++ Bouclewhileexercice3/exercice3.js
@@ -2,10 +2,10 @@
 
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
-var i = 0;
-while (i < 15) {
+var i = 20;
+while (i > 0) {
     document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i;
-    i++;
+    i = i -1  ;
 }
 
 
---
+++ Bouclewhileexercice4/exercice4.js
@@ -0,0 +1,11 @@
+
+
+/* Modifier le code ci dessous pour réaliser l'exercice */
+
+var i = 15;
+while (i > 0  ) {
+    document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i;
+   i =  i - 5 ;
+}
+
+
