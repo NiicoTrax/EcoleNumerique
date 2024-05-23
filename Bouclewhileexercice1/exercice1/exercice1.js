b823b99c66813f00ba0c8fbab64cf7c69a851f6e
NiicoTrax <nicolaslegaye@live.fr>
2024-05-23 10:55:26 +0200

    good

--- Bouclewhileexercice1/exercice1/exercice1.js
+++ Bouclewhileexercice1/exercice1/exercice1.js
@@ -1,6 +1,15 @@
 
 
-//Votre code ci dessous
+// l'élément image du chat
+let catPic = document.getElementById('cat-pic');
+
+//  fonction de retour
+function catMeow() {
+    document.getElementById('cat-chat').innerText = 'Miaou';
+}
+
+// fonction de retour comme écouteur d'événement
+catPic.addEventListener('click', catMeow);
 
 
 
