8cb5d6fb7cb7da03d147443338e076f3cc846da5
NiicoTrax <nicolaslegaye@live.fr>
2024-05-27 14:01:25 +0200

    Good

---
+++ chronometreexcercice1/exercice1.js
@@ -0,0 +1,12 @@
+// Make it count up on a timer, calling this function
+var seconds = document.getElementById("seconds");
+var countUp = function() {
+    // ?
+};
+
+// How can you make it stop counting?
+var stopCountUp = function() {
+};
+var stopButton = document.getElementById("stop-button");
+stopButton.addEventListener("click", stopCountUp);
+
