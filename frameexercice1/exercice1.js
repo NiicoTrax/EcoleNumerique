5e45b2756e7a52c6b05a0ac51118209dab70e4a0
NiicoTrax <nicolaslegaye@live.fr>
2024-05-27 13:56:22 +0200

    Good

---
+++ frameexercice1/exercice1.js
@@ -0,0 +1,8 @@
+var catEl = document.getElementById("cat");
+
+var startTime = new Date().getTime();
+var walkTheCat = function() {
+    var currTime = new Date().getTime();
+    var secondsElapsed = ((currTime - startTime)/1000);
+};
+walkTheCat();
