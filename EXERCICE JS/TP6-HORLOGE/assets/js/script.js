36bf3f1d4aaaf2393ea3d457de0bc6310cfa38f1
NiicoTrax <nicolaslegaye@live.fr>
2024-06-01 19:47:02 +0200

    TP HORLOGE FINI

--- EXERCICE JS/TP6-HORLOGE/assets/js/script.js
+++ EXERCICE JS/TP6-HORLOGE/assets/js/script.js
@@ -1,26 +1,26 @@
 // Initialize the current time
-var temps = new Date();
-var time = temps.getSeconds() + 60 * temps.getMinutes() + 3600 * temps.getHours();
-var donneHeure = temps.getHours();
-var donneMinute = temps.getMinutes();
-var donneSc= temps.getSeconds(); 
+var currentTime = new Date();
+var time = currentTime.getSeconds() + 60 * currentTime.getMinutes() + 3600 * currentTime.getHours();
+var hour = currentTime.getHours();
+var minute = currentTime.getMinutes();
+var second = currentTime.getSeconds(); 
 
-// Set IDs for the clock hand elements
-var tempsSeconde = document.getElementById("seconde");
-var tempsMinute = document.getElementById("minute");
-var tempsHeure = document.getElementById("heure");
+// Define IDs for the clock hands elements
+var secondHand = document.getElementById("seconde");
+var minuteHand = document.getElementById("minute");
+var hourHand = document.getElementById("heure");
 
-// Rotate clock hands based on the initial time
-tempsSeconde.style.transform = "rotate(" + (time * 6) + "deg)";
-tempsMinute.style.transform = "rotate(" + Math.round(time / 10) + "deg)";
-tempsHeure.style.transform = "rotate(" + Math.round(time / 120) + "deg)";
+// Rotate the hands based on the initial time
+secondHand.style.transform = "rotate(" + (time * 6) + "deg) translateX(-50%)";
+minuteHand.style.transform = "rotate(" + Math.round(time / 10) + "deg) translateX(-50%)";
+hourHand.style.transform = "rotate(" + Math.round(time / 120) + "deg) translateX(-50%)";
 
-// Update the clock hands every second
+// Update the hands every second
 setInterval(function() {
-    // Increment time by one second
+    // Increment the time by one second
     time++;
-    // Update the rotation of the second, minute, and hour hands
-    tempsSeconde.style.transform = "rotate(" + (time * 6) + "deg)";
-    tempsMinute.style.transform = "rotate(" + Math.round(time / 10) + "deg)";
-    tempsHeure.style.transform = "rotate(" + Math.round(time / 120) + "deg)";
-},1000);
+    // Update the rotation of the hands
+    secondHand.style.transform = "rotate(" + (time * 6) + "deg) translateX(-50%)";
+    minuteHand.style.transform = "rotate(" + Math.round(time / 10) + "deg) translateX(-50%)";
+    hourHand.style.transform = "rotate(" + Math.round(time / 120) + "deg) translateX(-50%)";
+}, 1000);
