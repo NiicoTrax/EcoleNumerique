ef41fb3afd930de2d1c0f724a3d6ee4a98b91228
NiicoTrax <nicolaslegaye@live.fr>
2024-06-06 21:26:21 +0200

    Exercice Terminé

---
+++ EXERCICE JS/JQuery/JQuerySelecteurEx4/Correction/exercice4.js
@@ -0,0 +1,23 @@
+// Cache tout les elements html
+// de la page grâce à "body"
+$("body").hide();
+
+// Cache tout les elements html "h1" de
+// la page
+$("h1").hide();
+
+// Cache tout les elements html "h2" de
+// la page
+$("h2").hide();
+
+// Cache tout les elements html "p" de
+// la page
+$("p").hide();
+
+// Cache tout les elements html "div" de
+// la page
+$("div").hide();
+
+// Cache tout les elements html "button"
+// de la page
+$("button").hide();
