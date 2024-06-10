b85fc0148ed1acefc01dfa588f9e82e21d811ff0
NiicoTrax <nicolaslegaye@live.fr>
2024-06-10 15:33:35 +0200

    Exercice 5 Terminer

---
+++ PHP24/Exercice5-Leschainesdecaractères/index.php
@@ -0,0 +1,52 @@
+<?php
+
+
+// Compléter le code ci dessous pour afficher la longueur de la chaine de caracteres
+
+ echo      ("google");
+
+    //echo"<br><br>"; //Decommentez les <br> pour tester l'affichage lorsque vous aurez plusieurs instructions
+
+ // Compléter le code ci dessous pour inverser la chaine de caractere
+
+echo  ("anticonstitutionellement");
+
+// Et si vous testiez également avec votre prénom ?
+
+
+
+  //echo"<br><br>";
+
+// Compléter le code ci dessous pour remplacer le mot ok par non
+$oldtxt = "Ok Google!";
+$newtxt = (, , );
+
+echo $newtxt;
+
+
+
+
+<?php
+
+// Compléter le code ci-dessous pour afficher la longueur de la chaîne de caractères
+echo strlen("google");
+
+echo "<br><br>"; // Décommentez les <br> pour tester l'affichage lorsque vous aurez plusieurs instructions
+
+// Compléter le code ci-dessous pour inverser la chaîne de caractères
+echo strrev("anticonstitutionnellement");
+
+echo "<br><br>";
+
+// Et si vous testiez également avec votre prénom ?
+echo strrev("Nicolas");
+
+echo "<br><br>";
+
+// Compléter le code ci-dessous pour remplacer le mot ok par non
+$oldtxt = "Ok Google!";
+$newtxt = str_replace("Ok", "non", $oldtxt);
+
+echo $newtxt;
+
+?>
