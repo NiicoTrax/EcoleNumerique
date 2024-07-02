765d4c35b9a9d2d2cd181e171664fb85dd2ceb12
NiicoTrax <nicolaslegaye@live.fr>
2024-07-02 11:12:57 +0200

    Placement de fichier erronné

--- PHP24/PROJET4/Enonce/footer.php
+++
@@ -1,8 +0,0 @@
-</div> <!-- fin de .container -->
-    </div> <!-- fin de .main-container -->
-    <footer>
-        <p>&copy; 2024 Niico. Tous droits réservés.</p>
-    </footer>
-    <script src="script.js"></script>
-</body>
-</html>
