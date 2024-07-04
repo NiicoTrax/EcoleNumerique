6b598a5b4d3a8f91b2fab79ff83f68d06b9999e2
NiicoTrax <nicolaslegaye@live.fr>
2024-07-04 15:36:08 +0200

    ok

---
+++ PHP24/Exercice24-POOStrutils/classes/StrUtils.php
@@ -0,0 +1,35 @@
+<?php
+class StrUtils {
+    // Propriété privée pour stocker la chaîne de caractères
+    private $str;
+
+    // Constructeur pour initialiser la propriété str
+    public function __construct($str) {
+        $this->str = $str;
+    }
+
+    // Méthode pour mettre en gras
+    public function bold() {
+        return '<strong>' . $this->str . '</strong>';
+    }
+
+    // Méthode pour mettre en italique
+    public function italic() {
+        return '<em>' . $this->str . '</em>';
+    }
+
+    // Méthode pour souligner
+    public function underline() {
+        return '<u>' . $this->str . '</u>';
+    }
+
+    // Méthode pour capitaliser
+    public function capitalize() {
+        return ucwords($this->str);
+    }
+
+    // Méthode pour appliquer du gras, de l'italique et du souligné
+    public function uglify() {
+        return $this->bold() . ' ' . $this->italic() . ' ' . $this->underline();
+    }
+}
---
+++ PHP24/Exercice24-POOStrutils/index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+require "classes/StrUtils.php";
+
+$utils = new StrUtils("Salut à tous");
+
+Echo "Bold : ".$utils->bold();
+
+Echo "<br><br>";
+
+Echo "Italic : ".$utils->italic();
+
+Echo "<br><br>";
+
+Echo "Underline : ".$utils->underline();
+
+Echo "<br><br>";
+
+Echo "Capitalize : ".$utils->capitalize();
+
+Echo "<br><br>";
+
+Echo "Bold, Italic, Underline, Capitalize : ".$utils->uglify();
